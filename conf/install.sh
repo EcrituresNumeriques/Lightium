59158427b5231c149d48fd5293d42834b659d457
root <root@ns520256.ip-198-27-68.net>
2016-02-05 09:58:17 -0500

    New install script

--- conf/install.sh
+++ conf/install.sh
@@ -2,11 +2,11 @@
 apt-get update
 apt-get upgrade
 apt-get install apache2 php5 php5-sqlite php5-curl php5-mcrypt
-rm /var/www/html/index.php
+rm /var/www/html/index.html
 a2enmod rewrite
 php5enmod mcrypt
-cp /config/apache2.conf /etc/apache2/apache2.conf
-cp /config/php.ini /etc/php5/apache2/php.ini
-cp /config/000-default.conf /etc/apache2/sites-available/000-default.conf
+cp apache2.conf /etc/apache2/apache2.conf
+cp php.ini /etc/php5/apache2/php.ini
+cp 000-default.conf /etc/apache2/sites-available/000-default.conf
 #Add the https version here + all the ssl generation etc
 service apache2 start
